e2acd03112f1c960aab2e47936dd1c08ae07f52a
birodani3 <birodani3@gmail.com>
2019-06-16 17:20:29 +0200

    Ensure that only https pages are loaded inside the iframe

--- src/app/components/url-prompt/url-prompt.component.ts
+++ src/app/components/url-prompt/url-prompt.component.ts
@@ -13,7 +13,7 @@ import { AppState } from '@store';
 })
 export class UrlPromptComponent {
     // tslint:disable-next-line: max-line-length
-    private readonly URL_REGEXP = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+    private readonly URL_REGEXP = /^(?:https:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/;
     public urlFormControl: FormControl;
 
     constructor(private store: Store<AppState>) {
@@ -24,7 +24,13 @@ export class UrlPromptComponent {
     }
 
     public setUrl(): void {
-        const url = this.urlFormControl.value;
+        let url: string = this.urlFormControl.value;
+
+        // The 'https://' prefix is necessary for the iFrame.
+        // We are not checking for 'http' prefixes here, because the RegExp filters those urls out.
+        if (!url.startsWith('https://')) {
+            url = `https://${url}`;
+        }
 
         if (this.urlFormControl.valid) {
             this.store.dispatch(urlChangeAction({ url }));
